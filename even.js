1ef089859596412c8f8a2ae7290e7f3707df177a
arjun-saseendran <arjunsaseendran@icloud.com>
2024-07-08 12:11:18 +0530

    created function for display even numbers form given limit

--- even.js
+++ even.js
@@ -1,5 +1,5 @@
 // using normal function instead of arrow function for hoisting
-function getEvenNumbers (limit) {
+function getEvenNumbers(limit) {
     //  for iteration until the limit
     for (let i = 0; i <= limit; i++) {
         // checking when i dividing by 2 the remainder is zero
@@ -9,6 +9,7 @@ function getEvenNumbers (limit) {
         }
     }
 }
+
 // calling function with limit number 11
 getEvenNumbers(11)
 
